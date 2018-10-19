8bf5a1759a9fec62f2bf0f88a2e7075ffecdccf1
Jeff Killinger <jeffkillinger@gmail.com>
2018-10-19 15:34:10 -0500

    basic about page

---
+++ src/components/pages/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <div>
+      <h1>About Task Rewards</h1>
+      <p>Here's how you do it:</p>
+      <p>Think of a task, assign points, do it, earn points</p>
+    </div>
+  );
+};
